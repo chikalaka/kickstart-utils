8da5abe459dd53778fe0da384a558c3639320087
Yael Chetrit <yael@Yaels-Air.lan>
2022-01-11 01:11:21 +0200

    added more basic functionality

--- index.js
+++ index.js
@@ -13,6 +13,31 @@ export const isString = isTypeof("string")
 export const isFunction = isTypeof("function")
 export const isUndefined = isTypeof("undefined")
 export const isBoolean = isTypeof("boolean")
+export const { isArray } = Array
+export const isError = e =>
+    (e instanceof Error) & (typeof e.message !== "undefined")
 
 export const cn = (...classes) =>
     classes.filter(identity).filter(isString).join(" ")
+
+export const noop = () => {}
+
+export const toArray = value => {
+    isArray(value) ? value : [value].filter(identity)
+}
+
+export const toDictionary = (arr, key) =>
+    isArray(arr) ? Object.fromEntries(arr.map((v, i) => [v[key] || i, o])) : {}
+
+export const stopEventPropagation = event => event.stopEventPropagation()
+
+const isObjectEmpty = obj => {
+    for (var i in obj) return false
+    return true
+}
+
+export const isEmpty = v => {
+    if (!v) return true
+    if (isString(v)) return !/([^\s])/.test(v)
+    return isObjectEmpty(v)
+}
