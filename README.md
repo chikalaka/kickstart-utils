147c879f158c76babaab95e764e7e75d0926cbc2
edanc <edanc@nsogroup.com>
2022-02-15 21:35:39 +0200

    - added prettier - added explanations on README

--- README.md
+++ README.md
@@ -1,37 +1,160 @@
-# kickstart-utils
+# [kickstart-utils](https://www.npmjs.com/package/kickstart-utils) [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/chikalaka/kickstart-utils/blob/main/LICENSE)
+
 Every project should kickstart with some basic utils :)
 
-## Usage
-`npm install kickstart-utils`
-
+With no dependencies! 
+## Installation
+```shell
+npm install kickstart-utils
 ```
-import { run, match, cn, toArray, toDictionary, isEmpty } from 'kickstart-utils'
 
-let myFunc = null
-run(myFunc) // null
+## API
 
-myFunc = () => console.log("hello")
-run(myFunc) // "hello"
+- [Utils](#utils)
+    - [cn](#cn)
+    - [isEmpty](#isEmpty)
+    - [match](#match)
+    - [noop](#noop)
+    - [random](#random)
+    - [run](#run)
+    - [stopEventPropagation](#stopEventPropagation)
+    - [toArray](#toArray)
+    - [toDictionary](#toDictionary)
+- [Type check](#type-check) []
+    - [isArray](#isArray)
+    - [isBoolean](#isBoolean)
+    - [isError](#isError)
+    - [isFunction](#isFunction)
+    - [isInteger](#isInteger)
+    - [isNullish](#isNullish)
+    - [isNumber](#isNumber)
+    - [isObject](#isObject)
+    - [isString](#isString)
+- [Types](#types) {}
+    - [Nullish](#Nullish)
+    - [Primitive](#Primitive)
 
-let switchExpression = "foo"
-const value = match(switchExpression , {
+### Utils
+
+### cn (class-names)
+Returns a string removing anything unrelated to css class names.
+```typescript jsx
+import { cn } from 'kickstart-utils';
+
+<div class={cn('foo', a && 'bar', b, 'baz')} /> // class="foo baz" 
+```
+
+### isEmpty
+Check if falsy (except 0) or empty object
+```js
+import { isEmpty } from 'kickstart-utils';
+
+isEmpty(null) // true
+isEmpty(" ") // true
+isEmpty([]) // true
+isEmpty({}) // true
+```
+
+### match
+Instead of cumbersome switch case, match for cleaner, reusable code
+```js
+import { match } from 'kickstart-utils';
+
+const value = match("foo" , {
   foo: 2,
   bar: 3,
   default: 5
 }) // 2
-
-let condition = false
-let classFromProp = undefined
-const chainedCSSClasses = cn("a", classFromProp, "b", condition && "c", "d") // "a b d"
-
-let myVar = 3
-toArray(myVar) // [3]
-toArray([myVar]) // [3]
-
-let array = [{ id: "1_2", foo: 2 }, { id: "3_4", bar: 3 }]
-toDictionary(array, "id") // { 1_2: {id: "1_2", foo: 2 }, 3_4: { id: "3_4", bar: 3 } }
-
-isEmpty([]) // true
-isEmpty({}) // true
-isEmpty("  ") // true
 ```
+
+### noop
+For any time we need a noop function
+```jsx
+import { noop } from 'kickstart-utils';
+
+<div onClick={noop} /> 
+```
+
+### random
+Random, or random between two numbers
+```js
+import { random } from 'kickstart-utils'
+
+random(4, 8) // 4, 5, 6, 7, 8
+```
+
+### run
+For the times we're not sure if the value is a function.
+For most cases, if we are sure the value is either a function or undefined, we can use `myFunc?.(args)`.
+```js
+import { run } from 'kickstart-utils';
+
+const myFunc = (text) => console.log('hi', text)
+run(myFunc, 'foo') // 'hi foo'
+run('Not a function') // 'Not a function'
+```
+
+### stopEventPropagation
+Instead of `event => event.stopPropagation()`
+```jsx
+import { stopEventPropagation } from 'kickstart-utils';
+
+<input onChange={stopEventPropagation} />
+```
+
+### toArray
+Convert anything to an array
+```js
+import { toArray } from 'kickstart-utils';
+
+toArray(null) // [null]
+toArray('hello') // ['hello']
+toArray([1, 2, 3]) // [1, 2, 3]
+```
+
+### toDictionary
+Convert array of objects to a dictionary
+```js
+import { toDictionary } from 'kickstart-utils';
+
+const arr = [{ id: 1, name: 'foo' }, { id: 2, name: 'bar' }]
+toDictionary(arr, 'id') // { 1: { id: 1, name: 'foo' }, 2: { id: 2, name: 'bar' } }
+```
+
+### Type check (with ts type guards)
+Most of them are self-explanatory.
+### isArray
+
+### isBoolean
+
+### isError
+
+### isFunction
+
+### isInteger
+With an option to check on a string.
+
+### isNullish
+Nullish is a type that is either null or undefined.
+
+### isNumber
+With an option to check on a string.
+
+### isObject
+Check if is an object (not Array, or null)
+```js
+import { isObject } from 'kickstart-utils';
+
+isObject([{ foo: 'bar' }]) // false
+```
+
+### isString
+
+### Types
+Some common ts types
+
+### Nullish
+null or undefined
+
+### Primitive
+js primitive types
--- src/test.ts
+++ src/test.ts
@@ -1,236 +1,253 @@
 import {
-    isBoolean,
-    isFunction,
-    isNullish,
-    isObject,
-    isString,
-    match,
-    run,
-    isArray,
-    isError,
-    isNumber,
-    isInteger, cn, noop, toArray, toDictionary, stopEventPropagation, isEmpty
-} from "./index";
+  isBoolean,
+  isFunction,
+  isNullish,
+  isObject,
+  isString,
+  match,
+  run,
+  isArray,
+  isError,
+  isInteger,
+  cn,
+  noop,
+  toArray,
+  toDictionary,
+  stopEventPropagation,
+  isEmpty,
+  isNumber
+} from "./index"
 
-describe("run", ()=>{
-    it("check values that are not a function", () => {
-        expect(run("abc", 1,2,3)).toBe("abc")
-        expect(run(null, 1,2,3)).toBe(null)
-        expect(run(undefined, 1,2,3)).toBe(undefined)
-        expect(run(23)).toBe(23)
-        expect(run(true)).toBe(true)
-        expect(run({a:1})).toEqual({a:1})
-    })
-    it("check on functions", () =>{
-        expect(run(()=>2)).toBe(2)
-        expect(run((v)=>v,2)).toBe(2)
-        expect(run((a,b,c)=>a+b+c,1,2,3)).toBe(6)
-    })
+describe("run", () => {
+  it("check values that are not a function", () => {
+    expect(run("abc", 1, 2, 3)).toBe("abc")
+    expect(run(null, 1, 2, 3)).toBe(null)
+    expect(run(undefined, 1, 2, 3)).toBe(undefined)
+    expect(run(23)).toBe(23)
+    expect(run(true)).toBe(true)
+    expect(run({ a: 1 })).toEqual({ a: 1 })
+  })
+  it("check on functions", () => {
+    expect(run(() => 2)).toBe(2)
+    expect(run(v => v, 2)).toBe(2)
+    expect(run((a, b, c) => a + b + c, 1, 2, 3)).toBe(6)
+  })
 })
 
-describe("match", () =>{
-    it("simple",()=>{
-        let switchExpression = "foo"
-        const switchObject = {
-            foo: 2,
-            bar: 3,
-            default: 5
-        }
-        expect(match("foo" , switchObject)).toBe(2)
-        expect(match("default" , switchObject)).toBe(5)
-        expect(match("not exist" , switchObject)).toBe(5)
-        expect(match("not exist" , switchObject)).toBe(5)
-        expect(match(2 , switchObject)).toBe(5)
-        expect(match(2 , {})).toBe(undefined)
-    })
-    it ("array", () =>{
-        expect(match(2 , [1,2,3])).toBe(3)
-    })
-})
-
-test("isObject", ()=>{
-    expect(isObject(null)).toBe(false)
-    expect(isObject("hello")).toBe(false)
-    expect(isObject(String("hello"))).toBe(false)
-    expect(isObject(new Object([1,2]))).toBe(false)
-    expect(isObject([{a:1}])).toBe(false)
-
-    expect(isObject(new Object({a:1}))).toBe(true)
-    expect(isObject(new Object({}))).toBe(true)
-    expect(isObject({a:1})).toBe(true)
-})
-
-test("isString",()=>{
-    expect(isString(undefined)).toBe(false)
-    expect(isString(null)).toBe(false)
-    expect(isString([1,2])).toBe(false)
-
-    expect(isString("")).toBe(true)
-    expect(isString("abc")).toBe(true)
-    expect(isString(String("abc"))).toBe(true)
-})
-
-test("isFunction",()=>{
-    expect(isFunction(undefined)).toBe(false)
-    expect(isFunction(null)).toBe(false)
-    expect(isFunction([1,2])).toBe(false)
-
-    expect(isFunction(()=>{})).toBe(true)
-})
-
-test("isNullish",()=>{
-    expect(isNullish([1,2])).toBe(false)
-    expect(isNullish("")).toBe(false)
-
-    expect(isNullish(undefined)).toBe(true)
-    expect(isNullish(null)).toBe(true)
-})
-
-test("isBoolean",()=>{
-    expect(isBoolean(undefined)).toBe(false)
-    expect(isBoolean(null)).toBe(false)
-    expect(isBoolean("")).toBe(false)
-    expect(isBoolean(0)).toBe(false)
-
-    expect(isBoolean(true)).toBe(true)
-    expect(isBoolean(false)).toBe(true)
-    expect(isBoolean(Boolean("false"))).toBe(true)
-    expect(isBoolean(Boolean(false))).toBe(true)
-})
-
-test("isArray",()=>{
-    expect(isArray(null)).toBe(false)
-    expect(isArray({a:1})).toBe(false)
-
-    expect(isArray([1,2])).toBe(true)
-    expect(isArray(Array(1,2))).toBe(true)
-})
-
-test("isError",()=>{
-    expect(isError(undefined)).toBe(false)
-    expect(isError(null)).toBe(false)
-    expect(isError(new Object({message:"msg"}))).toBe(false)
-
-    expect(isError(Error())).toBe(true)
-    expect(isError(new Error("hi"))).toBe(true)
-})
-
-test("isNumber",()=>{
-    expect(isNumber(undefined)).toBe(false)
-    expect(isNumber(null)).toBe(false)
-    expect(isNumber([1])).toBe(false)
-    expect(isNumber("2")).toBe(false)
-    expect(isNumber(NaN)).toBe(false)
-    expect(isNumber(NaN, true)).toBe(false)
-    expect(isNumber("123..2144", true)).toBe(false)
-    expect(isNumber("123.21.44", true)).toBe(false)
-    expect(isNumber(".123.2144", true)).toBe(false)
-    expect(isNumber(".1232144.", true)).toBe(false)
-
-    expect(isNumber(123.2144)).toBe(true)
-    expect(isNumber("123.2144", true)).toBe(true)
-    expect(isNumber(".1232144", true)).toBe(true)
-    expect(isNumber("1232144.", true)).toBe(true)
-    expect(isNumber("0.1232144", true)).toBe(true)
-    expect(isNumber("1232144.0", true)).toBe(true)
-})
-
-test("isInteger",()=>{
-    expect(isInteger(undefined)).toBe(false)
-    expect(isInteger([1])).toBe(false)
-    expect(isInteger("2")).toBe(false)
-    expect(isInteger(NaN)).toBe(false)
-    expect(isInteger(NaN, true)).toBe(false)
-    expect(isInteger(123.2144)).toBe(false)
-    expect(isInteger(".1232144", true)).toBe(false)
-
-    expect(isInteger("123.", true)).toBe(true)
-    expect(isInteger("123.0", true)).toBe(true)
-    expect(isInteger(123.0)).toBe(true)
-    expect(isInteger("1232144", true)).toBe(true)
-    expect(isInteger(1232144, true)).toBe(true)
-})
-
-test("cn", ()=>{
-    const arr = [
-        "a",()=>"a",["a"],{a:"a"},true,false,undefined,null,2,NaN,"b","c"
-    ]
-    // @ts-ignore
-    expect(cn(...arr)).toBe("a b c")
-})
-
-test("noop",()=>{
-    expect(noop).toBe(noop)
-})
-
-test("toArray",()=>{
-    expect(toArray(undefined)).toEqual([undefined])
-    expect(toArray([undefined])).toEqual([undefined])
-    expect(toArray(0)).toEqual([0])
-    expect(toArray([0])).toEqual([0])
-    expect(toArray(false)).toEqual([false])
-    expect(toArray([false])).toEqual([false])
-    expect(toArray(2)).toEqual([2])
-    expect(toArray([2])).toEqual([2])
-    expect(toArray([[]])).toEqual([[]])
-    expect(toArray([])).toEqual([])
-})
-
-test("toDictionary",()=>{
-    const arr = [
-        {id:1,name:"aaa"},
-        {id:"hi",name:"bbb"},
-        {id:3,name:"ccc"}
-    ]
-    const expected = {
-        1: {id:1,name:"aaa"},
-        "hi": {id:"hi",name:"bbb"},
-        3: {id:3,name:"ccc"}
+describe("match", () => {
+  it("simple", () => {
+    const switchObject = {
+      foo: 2,
+      bar: 3,
+      default: 5
     }
-    expect(toDictionary(arr,"id")).toEqual(expected)
-    expect(toDictionary([],"id")).toEqual({})
+    expect(match("foo", switchObject)).toBe(2)
+    expect(match("default", switchObject)).toBe(5)
+    expect(match("not exist", switchObject)).toBe(5)
+    expect(match("not exist", switchObject)).toBe(5)
+    expect(match(2, switchObject)).toBe(5)
+    expect(match(2, {})).toBe(undefined)
+  })
+  it("array", () => {
+    expect(match(2, [1, 2, 3])).toBe(3)
+  })
 })
 
-test("stopEventPropagation",()=>{
-    expect(stopEventPropagation(undefined)).toBe(undefined)
-    expect(stopEventPropagation(null)).toBe(undefined)
+test("isObject", () => {
+  expect(isObject(null)).toBe(false)
+  expect(isObject("hello")).toBe(false)
+  expect(isObject(String("hello"))).toBe(false)
+  expect(isObject(new Object([1, 2]))).toBe(false)
+  expect(isObject([{ a: 1 }])).toBe(false)
+
+  expect(isObject(new Object({ a: 1 }))).toBe(true)
+  expect(isObject(new Object({}))).toBe(true)
+  expect(isObject({ a: 1 })).toBe(true)
 })
 
-describe("isEmpty",()=>{
-    it("string",()=>{
-        expect(isEmpty(String("?"))).toBe(false)
-        expect(isEmpty("false")).toBe(false)
-        expect(isEmpty("    ")).toBe(true)
-    })
-    it("object",()=>{
-        expect(isEmpty({a:{}})).toBe(false)
-        expect(isEmpty({a:1})).toBe(false)
-        expect(isEmpty({})).toBe(true)
-        expect(isEmpty(new Object({}))).toBe(true)
-    })
-    it("array",()=>{
-        expect(isEmpty([null])).toBe(false)
-        expect(isEmpty([undefined])).toBe(false)
-        expect(isEmpty([" "])).toBe(false)
-        expect(isEmpty(Array(1,2))).toBe(false)
-        expect(isEmpty(Array())).toBe(true)
-        expect(isEmpty([])).toBe(true)
-    })
-    it("Error",()=>{
-        expect(isEmpty(new Error())).toBe(false)
-        expect(isEmpty(new Error("err"))).toBe(false)
-    })
-    it("Number",()=>{
-        expect(isEmpty(Number(23))).toBe(false)
-        expect(isEmpty(Infinity)).toBe(false)
-        expect(isEmpty(0)).toBe(false)
-        expect(isEmpty(NaN)).toBe(true)
-    })
-    it("function", ()=>{
-        expect(isEmpty(() => {})).toBe(false)
-    })
-    it("boolean", ()=>{
-        expect(isEmpty(Boolean(true))).toBe(false)
-        expect(isEmpty(false)).toBe(true)
-    })
+test("isString", () => {
+  expect(isString(undefined)).toBe(false)
+  expect(isString(null)).toBe(false)
+  expect(isString([1, 2])).toBe(false)
+
+  expect(isString("")).toBe(true)
+  expect(isString("abc")).toBe(true)
+  expect(isString(String("abc"))).toBe(true)
+})
+
+test("isFunction", () => {
+  expect(isFunction(undefined)).toBe(false)
+  expect(isFunction(null)).toBe(false)
+  expect(isFunction([1, 2])).toBe(false)
+
+  expect(isFunction(() => {})).toBe(true)
+})
+
+test("isNullish", () => {
+  expect(isNullish([1, 2])).toBe(false)
+  expect(isNullish("")).toBe(false)
+
+  expect(isNullish(undefined)).toBe(true)
+  expect(isNullish(null)).toBe(true)
+})
+
+test("isBoolean", () => {
+  expect(isBoolean(undefined)).toBe(false)
+  expect(isBoolean(null)).toBe(false)
+  expect(isBoolean("")).toBe(false)
+  expect(isBoolean(0)).toBe(false)
+
+  expect(isBoolean(true)).toBe(true)
+  expect(isBoolean(false)).toBe(true)
+  expect(isBoolean(Boolean("false"))).toBe(true)
+  expect(isBoolean(Boolean(false))).toBe(true)
+})
+
+test("isArray", () => {
+  expect(isArray(null)).toBe(false)
+  expect(isArray({ a: 1 })).toBe(false)
+
+  expect(isArray([1, 2])).toBe(true)
+  expect(isArray(Array(1, 2))).toBe(true)
+})
+
+test("isError", () => {
+  expect(isError(undefined)).toBe(false)
+  expect(isError(null)).toBe(false)
+  expect(isError("Error")).toBe(false)
+  expect(isError(new Object({ message: "msg" }))).toBe(false)
+
+  expect(isError(Error())).toBe(true)
+  expect(isError(new Error("hi"))).toBe(true)
+})
+
+test("isNumber", () => {
+  expect(isNumber(undefined)).toBe(false)
+  expect(isNumber(null)).toBe(false)
+  expect(isNumber([1])).toBe(false)
+  expect(isNumber("2")).toBe(false)
+  expect(isNumber(NaN)).toBe(false)
+  expect(isNumber(NaN, true)).toBe(false)
+  expect(isNumber("123..2144", true)).toBe(false)
+  expect(isNumber("123.21.44", true)).toBe(false)
+  expect(isNumber(".123.2144", true)).toBe(false)
+  expect(isNumber(".1232144.", true)).toBe(false)
+
+  expect(isNumber(123.2144)).toBe(true)
+  expect(isNumber("123.2144", true)).toBe(true)
+  expect(isNumber(".1232144", true)).toBe(true)
+  expect(isNumber("1232144.", true)).toBe(true)
+  expect(isNumber("0.1232144", true)).toBe(true)
+  expect(isNumber("1232144.0", true)).toBe(true)
+})
+
+test("isInteger", () => {
+  expect(isInteger(undefined)).toBe(false)
+  expect(isInteger([1])).toBe(false)
+  expect(isInteger("2")).toBe(false)
+  expect(isInteger(NaN)).toBe(false)
+  expect(isInteger(NaN, true)).toBe(false)
+  expect(isInteger(123.2144)).toBe(false)
+  expect(isInteger(".1232144", true)).toBe(false)
+
+  expect(isInteger("123.", true)).toBe(true)
+  expect(isInteger("123.0", true)).toBe(true)
+  expect(isInteger(123.0)).toBe(true)
+  expect(isInteger("1232144", true)).toBe(true)
+  expect(isInteger(1232144, true)).toBe(true)
+})
+
+test("cn", () => {
+  const arr = [
+    "a",
+    () => "a",
+    ["a"],
+    { a: "a" },
+    true,
+    false,
+    undefined,
+    null,
+    2,
+    NaN,
+    "b",
+    "c"
+  ]
+  // @ts-ignore
+  expect(cn(...arr)).toBe("a b c")
+})
+
+test("noop", () => {
+  expect(noop).toBe(noop)
+})
+
+test("toArray", () => {
+  expect(toArray(undefined)).toEqual([undefined])
+  expect(toArray([undefined])).toEqual([undefined])
+  expect(toArray(0)).toEqual([0])
+  expect(toArray([0])).toEqual([0])
+  expect(toArray(false)).toEqual([false])
+  expect(toArray([false])).toEqual([false])
+  expect(toArray(2)).toEqual([2])
+  expect(toArray([2])).toEqual([2])
+  expect(toArray([[]])).toEqual([[]])
+  expect(toArray([])).toEqual([])
+})
+
+test("toDictionary", () => {
+  const arr = [
+    { id: 1, name: "aaa" },
+    { id: "hi", name: "bbb" },
+    { id: 3, name: "ccc" }
+  ]
+  const expected = {
+    1: { id: 1, name: "aaa" },
+    hi: { id: "hi", name: "bbb" },
+    3: { id: 3, name: "ccc" }
+  }
+  expect(toDictionary(arr, "id")).toEqual(expected)
+  expect(toDictionary([], "id")).toEqual({})
+})
+
+test("stopEventPropagation", () => {
+  expect(stopEventPropagation(undefined)).toBe(undefined)
+  expect(stopEventPropagation(null)).toBe(undefined)
+})
+
+describe("isEmpty", () => {
+  it("string", () => {
+    expect(isEmpty(String("?"))).toBe(false)
+    expect(isEmpty("false")).toBe(false)
+    expect(isEmpty("    ")).toBe(true)
+  })
+  it("object", () => {
+    expect(isEmpty({ a: {} })).toBe(false)
+    expect(isEmpty({ a: 1 })).toBe(false)
+    expect(isEmpty({})).toBe(true)
+    expect(isEmpty(new Object({}))).toBe(true)
+  })
+  it("array", () => {
+    expect(isEmpty([null])).toBe(false)
+    expect(isEmpty([undefined])).toBe(false)
+    expect(isEmpty([" "])).toBe(false)
+    expect(isEmpty(Array(1, 2))).toBe(false)
+    expect(isEmpty(Array())).toBe(true)
+    expect(isEmpty([])).toBe(true)
+  })
+  it("Error", () => {
+    expect(isEmpty(new Error())).toBe(false)
+    expect(isEmpty(new Error("err"))).toBe(false)
+  })
+  it("Number", () => {
+    expect(isEmpty(Number(23))).toBe(false)
+    expect(isEmpty(Infinity)).toBe(false)
+    expect(isEmpty(0)).toBe(false)
+    expect(isEmpty(NaN)).toBe(true)
+  })
+  it("function", () => {
+    expect(isEmpty(() => {})).toBe(false)
+  })
+  it("boolean", () => {
+    expect(isEmpty(Boolean(true))).toBe(false)
+    expect(isEmpty(false)).toBe(true)
+  })
 })
